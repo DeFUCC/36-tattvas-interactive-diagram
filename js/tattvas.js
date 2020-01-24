64e99e041256763fe86b9b7324012f874830f0ad
DeFUCC <fond@fondfrukt.ru>
2020-01-24 22:35:44 +0300

    Треть таттв заполнил

--- js/tattvas.js
+++ js/tattvas.js
@@ -5,6 +5,36 @@ export const groups = {
     name:'Грубые элементы',
     text:'Преимущественно тамасичные',
   },
+  tanmatra: {
+    sans:'तन्मात्र',
+    trans:'TANMĀTRA',
+    name:'Тонкие элементы',
+    text:'Преимущественно тамасичные',
+  },
+  karmendriya: {
+    sans:'कर्मेन्द्रिय',
+    trans:'KARMENDRIYA',
+    name:'Силы действия',
+    text:'Преимущественно раджасичные',
+  },
+  jnanendriya: {
+    sans:'ज्ञानेन्द्रिय',
+    trans:'JÑĀNENDRIYA',
+    name:'Силы восприятия',
+    text:'Преимущественно саттвичные',
+  },
+  antahkarana: {
+    sans:'अन्तःकरण',
+    trans:'ANTAḤKARAṆA',
+    name:'Внутренний орган',
+    text:'Психический мир в целом.',
+  },
+  kancuka: {
+    sans:'कञ्चुक',
+    trans:'KAÑCUKA',
+    name:'Покровы',
+    text:'Ограничения, определяющие индивидуальное сознание',
+  },
 }
 
 export const bijas = {
@@ -34,12 +64,32 @@ export const tattvas = {
     group:'mahabhuta',
     bija:'vam'
   },
+  agni: {
+    num:34,
+    sans:'अग्नि',
+    trans:'Agni',
+    title:'Огонь',
+    text:'Жар. Плазма. Всё горячее, светящееся и обладающее цветом.',
+    group:'mahabhuta',
+    bija:'ram'
+  },
+  vayu: {
+    num:33,
+    sans:'वायु',
+    trans:'Vāyu',
+    title:'Воздух',
+    text:'Газ. Газообразное состояние вещества.',
+    group:'mahabhuta',
+    bija:'yam'
+  },
   akasha: {
     num:32,
     sans:'आकाश',
     trans:'Ākāśa',
     title:'Эфир',
     text:'Трёхмерный каркас, в котором существует весь физический мир — Пространство.',
+    group:'mahabhuta',
+    bija:'ham'
   },
   gandha: {
     num:31,
@@ -47,13 +97,50 @@ export const tattvas = {
     trans:'Gandha',
     title:'Запах',
     text:'Запах как таковой. Запах чистоты.',
+    group:'tanmatra',
+    bija:'lam'
   },
-
+  rasa: {
+    num:30,
+    sans:'रस',
+    trans:'Rasa',
+    title:'Вкус',
+    text:'Вкус как таковой. Вкус воды.',
+    group:'tanmatra',
+    bija:'vam'
+  },
+  rupa: {
+    num:29,
+    sans:'रूप',
+    trans:'Rūpa',
+    title:'Внешний вид',
+    text:'Форма, свет и цвет.',
+    group:'tanmatra',
+    bija:'ram'
+  },
+  sparsha: {
+    num:28,
+    sans:'स्पर्श',
+    trans:'Sparśa',
+    title:'Прикосновение',
+    text:'Соприкосновение как таковое. Прикосновение воздуха.',
+    group:'tanmatra',
+    bija:'yam'
+  },
+  shabda: {
+    num:27,
+    sans:'शब्द',
+    trans:'Śabda',
+    title:'Звук',
+    text:'Звук как таковой. Звук тишины.',
+    group:'tanmatra',
+    bija:'ham'
+  },
   sadashiva: {
     num:3,
     sans:'सदाशिव',
     trans:'Sadāśiva',
     title:'Садашива',
-    text:'обиталище Icchāśakti или Силы Воли. Здесь появляется туманная Вселенная. Сознание принимает форму “Я есть Это” (“Я — эта туманная Вселенная”). Вселенная только появляется и потому предстаёт размытой. Так как Вселенная ещё не сформировалась, “Я” преобладает.',
+    text:'Обиталище Icchāśakti или Силы Воли. Здесь появляется туманная Вселенная. Сознание принимает форму “Я есть Это” (“Я — эта туманная Вселенная”). Вселенная только появляется и потому предстаёт размытой. Так как Вселенная ещё не сформировалась, “Я” преобладает.',
   },
 }
