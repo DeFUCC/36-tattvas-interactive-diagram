d30f411231cace57d8a9d227469bfe85f3e12b4f
DeFUCC <fond@fondfrukt.ru>
2020-01-24 14:33:20 +0300

    first iteration

---
+++ js/app.js
@@ -0,0 +1,59 @@
+import tattvas from './tattvas.js'
+
+const ct = new Vue({
+  el:"#ananda",
+  components: {
+
+  },
+  data: {
+    url:'svg/clean-ananda.svg',
+    tattva:{},
+    svg:{},
+    selected:null,
+  },
+  methods: {
+    resize() {
+      this.svg.fitBounds(paper.view.bounds);
+    }
+  },
+  watch: {
+
+  },
+  mounted() {
+    paper.setup(document.getElementById('paper'));
+
+    paper.project.importSVG(this.url, (svg) => {
+      this.svg=svg;
+      paper.view.onResize = this.resize;
+      this.resize();
+
+      svg.getItems({class:paper.Path}).forEach( item => {
+
+          let light = item.fillColor.lightness;
+          let tween = {stop(){}};
+
+          item.onMouseEnter = (event) => {
+            tween.stop();
+            item.strokeWidth=1;
+            item.strokeColor='#fff';
+            tween = item.tween({'fillColor.lightness':Math.sqrt(light)}, {easing:'easeInOutCubic', duration:300});
+          }
+
+          item.onClick = (event) => {
+            this.tattva = item.id;
+            if (tattvas.hasOwnProperty(item.name)) {
+              this.tattva = tattvas[item.name]
+            }
+          }
+
+          item.onMouseLeave = (event) => {
+            item.strokeWidth=0;
+            tween.stop();
+            item.tweenTo({'fillColor.lightness':light}, {easing:'easeInOutCubic', duration:300})
+          }
+
+      })
+    })
+
+  },
+});
---
+++ js/tattvas.js
@@ -0,0 +1,34 @@
+const types= {
+  mahabhuta: {
+    sans:'महाभूतं',
+    trans:'MAHĀBHŪTA',
+    name:'Грубые элементы',
+    text:'Преимущественно тамасичные',
+  },
+}
+
+export default {
+
+  lam: {
+    sans:'लं',
+    trans:'laṃ',
+    title:'Лам',
+    text:'Пространство Земли',
+  },
+  prithivi: {
+    number:36,
+    sans:'पृथिवी',
+    trans:'Pṛthivī',
+    title:'Земля',
+    text:'Всё твердое',
+    type:types.mahabhuta,
+  },
+  gandha: {
+    number:31,
+    sans:'गन्ध',
+    trans:'Gandha',
+    title:'Запах',
+    text:'Запах как таковой. Запах чистоты.',
+  },
+
+}
